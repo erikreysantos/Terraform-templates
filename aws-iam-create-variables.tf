5e5c52465e92e581619d3c64d2a27edcd5b22d67
erikreysantos <santos.erik.rey@gmail.com>
2024-09-10 19:31:38 +0800

    Create aws-iam-create-variables.tf

---
+++ aws-iam-create-variables.tf
@@ -0,0 +1,24 @@
+variable "iam_user_name" {
+  description = "The name of the IAM user"
+  type = string
+}
+
+variable "aws_access_key" {
+  description = "Your access Key"
+  type     = string
+}
+
+variable "aws_secret_key" {
+  description = "Your secrete Key"
+  type     = string
+}
+
+variable "aws_region" {
+  description = "Your region"
+  type     = string
+}
+
+variable "initial_password" {
+  description = "Your region"
+  type     = string
+}
